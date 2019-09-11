792165de06be432e671476ac0f523711c9b8181b
rakesh <rakesh.kohli@mail.vinove.com>
2019-09-11 18:11:25 +0530

    home page desktop version

--- html/assets/js/custom.js
+++ html/assets/js/custom.js
@@ -20,17 +20,17 @@ $(document).ready(function(){
 
 });
 
-$(function(){
-  // Bind the swipeHandler callback function to the swipe event on div.box
-  $( "body" ).on( "swipe", swipeHandler );
-  // Callback function references the event target and adds the 'swipe' class to it
-  function swipeHandler( event ){
-    //$( event.target ).addClass( "swipe" );
-    //$( "body" ).addClass( "swipe" );
-    $(".app-wrapper").removeClass("moveLeft");
-    $(".before-login").removeClass("moveLeft");
-    $(".wrapper-normal").toggleClass("moveLeft");
-    $(".main-application").toggleClass("moveLeft");
-    $(".mobile-nav").toggleClass("show").toggleClass("hide");
-  }
-});
+// $(function(){
+//   // Bind the swipeHandler callback function to the swipe event on div.box
+//   $( "body" ).on( "swipe", swipeHandler );
+//   // Callback function references the event target and adds the 'swipe' class to it
+//   function swipeHandler( event ){
+//     //$( event.target ).addClass( "swipe" );
+//     //$( "body" ).addClass( "swipe" );
+//     $(".app-wrapper").removeClass("moveLeft");
+//     $(".before-login").removeClass("moveLeft");
+//     $(".wrapper-normal").toggleClass("moveLeft");
+//     $(".main-application").toggleClass("moveLeft");
+//     $(".mobile-nav").toggleClass("show").toggleClass("hide");
+//   }
+// });
