9d902a87e1232cd9edb5ca5f006519fdd99cfdd3
rakesh <rakesh.kohli@mail.vinove.com>
2020-01-31 15:09:24 +0530

    services.html

--- html/assets/js/custom.js
+++ html/assets/js/custom.js
@@ -12,19 +12,38 @@ $(document).ready(function(){
     });
 
 
-
-    //mobile menu
+//menu hover
+$(".app-menu-list").hover(function(){
+    $(".app-nav").toggleClass("hover-state");
+   });
+   //mobile footer menu
+   $(".app-footer h6").click(function(){
+     $(this).parent(".app-footer").siblings().find("nav").removeClass("show");
+     $(this).parent("nav").toggleClass("show");
+    });
+    $(".footer-logo").click(function(){
+     $(this).toggleClass("active");
+     $(".policy-links").toggleClass("show");
+    });
     $(".btn-mobileMenu").click(function(){
-    $("body").toggleClass("translate-x");
-    setTimeout(function(){
-    $(".has-subMenu > a").removeClass("active").next(".level-1").removeClass("show", 1000, "easeInBack");
-    }, 1000);
+     $("body").toggleClass("translate-x");
+     setTimeout(function(){
+       $(".has-subMenu > a").removeClass("active").next(".level-1").removeClass("show", 1000, "easeInBack");
+     }, 1000);
     });
     $(".offCanvas-nav button").click(function(){
-    $("body").toggleClass("translate-x");
-
+     $("body").toggleClass("translate-x");
+ 
+     setTimeout(function(){
+     $(".has-subMenu > a").removeClass("active").next(".level-1").removeClass("show", 1000, "easeInBack");
+   }, 1000);
+ 
     });
+    $(".has-subMenu > a").click(function(){
+     $(this).toggleClass("active").next(".level-1").toggleClass("show");
     });
+ });
+ 
 
 // $(function(){
 //   // Bind the swipeHandler callback function to the swipe event on div.box
